edc973a8c97eb8ab80ef0350ad88ab25eb06cc54
Filip Pawelec <pawelecfilip6@gmail.com>
2022-05-22 00:13:06 +0200

    Started working on data reader.

--- inc/data_reader.h
+++ inc/data_reader.h
@@ -5,4 +5,64 @@
 #ifndef KONSERWATORIUM_DATA_READER_H
 #define KONSERWATORIUM_DATA_READER_H
 
+#include <vector>
+#include <string>
+#include <map>
+#include <iostream>
+
+namespace reader {
+    class Node {
+    private:
+        std::string value;
+        uint64_t visit_count{};
+    public:
+        Node() = default;
+        explicit Node(std::string value){
+            this->value = value;
+        }
+        /*
+         * Getters
+         */
+        std::string get_value(){return this->value;}
+        uint64_t get_visit_count(){return this->visit_count;}
+        /*
+         * Setters
+         */
+        void increment_visit_count(){this->visit_count++;}
+
+    };
+
+    class Matrix {
+    private:
+        std::vector<std::vector<reader::Node>> m;
+    public:
+        Matrix() = default;
+        /*
+         * Parametric constructor, which takes rows of matrix as read from file, and creates a matrix
+         * of node objects.
+         */
+        Matrix(std::vector<std::vector<std::string>> file_contents);
+        /*
+         * Since the C++ standard is CPP_14, array subscript operator is implemented as follows.
+         */
+        auto &operator[](std::size_t row){return this->m[row];}
+
+        friend std::ostream &operator<<(std::ostream &os, const Matrix &obj);
+    };
+
+    class DataReader {
+    private:
+        /*
+         * Creating the object is intentionally not allowed.
+         */
+        DataReader() = default;
+
+    public:
+        /*
+         * Static method for extracting data from a given file.
+         */
+        static std::vector<std::vector<std::string>> read_csv_file(std::ifstream file_handle);
+    };
+}
+
 #endif //KONSERWATORIUM_DATA_READER_H
--- src/data_reader.cpp
+++ src/data_reader.cpp
@@ -2,4 +2,20 @@
  * This file contains definitions and implementations for anything contained in data_reader.h file.
  */
 /* TODO: rewrite cmake so that no relative path is used - only header name */
+#include <fstream>
 #include "../inc/data_reader.h"
+
+reader::Matrix::Matrix(std::vector<std::vector<std::string>> file_contents) {
+    for (auto row{0U}; row < file_contents.size(); row++) {
+        this->m.emplace_back(std::vector<reader::Node>());
+        for (auto node{0U}; node < file_contents[0].size(); node++) {
+            this->m[row].emplace_back(Node(file_contents[row][node]));
+        }
+    }
+}
+
+std::vector<std::vector<std::string>> reader::DataReader::read_csv_file(std::ifstream file_handle) {
+    // TODO: Implement
+    std::vector<std::vector<std::string>> buff{};
+    return buff;
+}
